f49cf565dc870cdc4eb45f44c2162a52d48d82c8
Alex Harris <alexharris@Alexs-MacBook-Air.local>
2023-09-21 17:46:08 -0700

    get cinematographer, screenwriter, cast, source

--- soup.py
+++ soup.py
@@ -9,13 +9,13 @@
 # Film Note Author Title
 # Legacy Credits
 # Director
-# Screenwriter
-# Cinematographer
-# Cast/With
+# Screenwriter x
+# Cinematographer x
+# Cast/With x
 # Country
 # Film Year
 # Film Run Time
-# Print Source
+# Print Source x
 # Permission
 # In Person Guest
 # Location
@@ -28,12 +28,14 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import csv
+import re
 # this lets us do a progress bar in the terminal
 from tqdm import tqdm 
 
 f = open('output.csv', "w")   
 
-test_nids = ['239940','238464','239764','239782','239789','196477','196491','196492','196494','196495','196508']
+# test_nids = ['239940','238464','239764','239782','239789','196477','196491','196492','196494','196495','196508']
+test_nids = ['239940']
 
 # get the real dataset from nodes.csv
 get_nids = open("nodes.csv", "r")
@@ -43,7 +45,7 @@ all_nids = data[0]
 
 #test_nids to test, all_nids to run 4 real
 for nid in tqdm(test_nids):
-    req = Request('https://bampfa.org/node/' + nid, headers={'User-Agent': 'Mozilla/5.0'})
+    req = Request('https://soup-bampfa-site.pantheon.berkeley.edu/node/' + nid, headers={'User-Agent': 'Mozilla/5.0'})
     html_page = urlopen(req).read()
 
     soup = BeautifulSoup(html_page, 'html.parser')
@@ -60,15 +62,34 @@ for nid in tqdm(test_nids):
         series = 'NOSERIES'
 
     # Long Description
-    get_long_desc = soup.select('.views-field-field-ao-curator-description .field-content p')
-    
+    get_long_desc = soup.select('.views-field-field-ao-curator-description .field-content p')    
     if len(get_long_desc)>0:
         long_desc = str(get_long_desc[0])
     else:
         long_desc = 'NOLONGDESCRIPTION'
 
-    print(long_desc)
-    
+    # Crew
+    screenwriter = ''
+    cinematographer = ''
+    source = ''
+
+    film_details = soup.select('.block-views-film-details-block .bampfa-accordion-content *')
+    for i in range(0,len(film_details)):
+        if(film_details[i].string == 'Screenwriter'):
+            for i in film_details[i+1]:
+                screenwriter += i.string.strip()
+        elif(film_details[i].string == 'Cinematographer'):
+            for i in film_details[i+1]:
+                cinematographer += i.string.strip() 
+        elif(film_details[i].string == 'Source'):
+            for i in film_details[i+1]:
+                source += i.string.strip()    
+
+    # Actors                           
+    body = soup.select('#node-event-full-group-body')
+    actors = body[0].select('.label-above')[0].find_next_siblings('section')[0].text.strip()
+    actors = re.sub('\s+',' ',actors)
+   
     # Put it all into output.csv
     for date in dates:
         f.write(title.string)
@@ -77,5 +98,13 @@ for nid in tqdm(test_nids):
         f.write(' | ')
         f.write(series) 
         f.write(' | ')
-        f.write(long_desc)                       
+        f.write(long_desc)
+        f.write(' | ')
+        f.write(screenwriter)       
+        f.write(' | ')
+        f.write(cinematographer)   
+        f.write(' | ')
+        f.write(source)    
+        f.write(' | ')
+        f.write(actors)                                                 
         f.write('\n')
